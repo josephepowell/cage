e0917b25dc2afa78728c68f37c63136d54cb7087
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-22 16:08:00 +1000

    Fix sample sorting

--- cleanEGCUT.R
+++ cleanEGCUT.R
@@ -17,35 +17,36 @@ info    <- read.table(file = file.path(inpath, "EGC_Proteomics_Vcodes_Pcodes_180
                     header = TRUE)
 PROBE_ID <- exp.raw[, 1]
 exp   <- exp.raw[, -1]
-# get Vcode from probe ID
+# get "V-code ID" from probe ID
 names <- sapply(colnames(exp), function(x) info[which(info[, 1] == x),2] )
 colnames(exp) <- names
-exp   <- exp[, order(colnames(exp))]
-exp   <- cbind(PROBE_ID, exp)
+# exp   <- exp[, order(colnames(exp))]
+# exp   <- cbind(PROBE_ID, exp)
 #---------------------------### Genotype data ###--------------------------------
 plink.1 <- read.plink(file.path(inpath, "EGCUT_370CNV_ExpSample_290814"))
-# Remove CNV genotypes that do not overlap with expression data
+# remove CNV genotypes that do not overlap with expression data
 gen.1   <- plink.1$genotypes@.Data
 gen.1   <- gen.1[which(rownames(gen.1) %in% names), ]
 fam.1   <- plink.1$fam
 fam.1   <- fam.1[rownames(gen.1), ]
-# All Omni genotypes overlap with expression data
+# all Omni genotypes overlap with expression data
 plink.2 <- read.plink(file.path(inpath, "EGCUT_OmniX_ExpSample_290814"))
 gen.2   <- plink.2$genotypes@.Data
 fam.2   <- plink.2$fam
-# Remove expression data for samples with no genotypes
+# remove expression data for samples with no genotypes
 names <- unique( c(rownames(plink.1$genotypes), rownames(plink.2$genotypes)) )
 names <- names[which(names %in% colnames(exp))]
 exp   <- exp[, names]
+exp   <- exp[, order(colnames(exp))]
 exp   <- cbind(PROBE_ID, exp)
-# TODO: Map probe IDs to ILMN_ID
+# Map probe IDs to ILMN_ID
 ill.path <- "/ibscratch/wrayvisscher/xander/CAGE/annotation/humanht-12_v3_0_r3_11283641_a_txt.zip"
 ill.anno <- read.delim(pipe(paste0("gunzip -c ", ill.path)),
                        skip = 8, header = TRUE)
 ill.ids  <- ill.anno[which(ill.anno$Array_Address_Id %in% exp$PROBE_ID),"Probe_Id"]
 exp$PROBE_ID <- ill.ids
 #-------------------------------------------------------------------------------
-# Perform outer join on genotype matrices, favouring Omni genotypes
+# perform outer join on genotype matrices, favouring Omni genotypes
 # names.1 <- rownames(gen.1)
 # names.2 <- rownames(gen.2)
 # gen.1 <- apply(gen.1, 2, as.numeric)
@@ -54,13 +55,16 @@ exp$PROBE_ID <- ill.ids
 # gen.2 <- data.frame(ID = names.2, gen.2)
 # gen <- plyr::join(gen.2, gen.1, type = "full", by = "ID")
 # rownames(gen) <- gen$ID
-# Strip ID row and column
+# strip ID row and column
 # gen <- gen[!rownames(gen) %in% "ID",!colnames(gen) %in% "ID"]
 # gen <- gen[order(rownames(gen)), ] # Sorting is undesirable because it breaks
 # gen <- gen[, order(colnames(gen))] # up the contiguous blocks of missing values
 # TODO: Merge fam tables
 # TODO: Merge map tables
 #-------------------------------------------------------------------------------
+# sort genotype matrices by sample ID
+gen.1 <- gen.1[order(rownames(gen.1)), ]
+gen.2 <- gen.2[order(rownames(gen.2)), ]
 # Create `SnpMatrix` objects
 gen.1 <- new("SnpMatrix", gen.1)
 gen.2 <- new("SnpMatrix", gen.2)
