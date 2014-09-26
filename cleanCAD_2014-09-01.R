9c7f6d1458e2e306595d7e4ab0677bb2d41cd004
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-26 11:58:59 +1000

    Resolve phase 2 expression data, sort all data for output

--- cleanCAD_2014-09-01.R
+++ cleanCAD_2014-09-01.R
@@ -23,23 +23,23 @@ PadWithZeroes <- function(id) {
 exp.raw.1 <- read.table(file = file.path(inpath, "Cardiology_phase1_raw.txt"),
                       header = TRUE,
                          sep = "\t")
+# phase 2 expression data is labelled with IDs in the form GG.0### for ### in (372:534)
+# these IDs match to the ColumnName in the covariate file
 exp.raw.2 <- read.table(file = file.path(inpath, "Cardiology_phase2_raw.txt"),
                       header = TRUE,
                          sep = "\t")
 # Extract probe info from phase 1 matrix
-PROBE_ID   <- exp.raw.1[, 1]
+PROBE_ID.1 <- exp.raw.1[, 1]
 exp.1      <- exp.raw.1[, order(colnames(exp.raw.1))]
 info.probe <- exp.1[, 1:10]
 exp.1      <- exp.1[, -c(1:11)] # strip probe info
-# TODO: check mapping of phase 2 IDs with CHDWB data
-# TODO: merge of expression data(???)
 # Extract STUDY_ID from phase 1 expression data
 exp.id <- colnames(exp.1)
 exp.id <- gsub("X[0-9]*_|\\.[A-z]*", "", exp.id)
 exp.id <- sapply(exp.id, PadWithZeroes)
 colnames(exp.1) <- exp.id
 #---------------------------### Genotype data ###-------------------------------
-gen.plink <- read.plink("/ibscratch/wrayvisscher/xander/CAGE/data/CAD/clean/CAD")
+gen.plink <- read.plink("/ibscratch/wrayvisscher/xander/CAGE/data/CAD/raw/genotypes/CAD")
 gen <- t(gen.plink$genotypes@.Data)
 # Extract STUDY_ID from genotype data
 gen.id <- gsub("X[0-9]*_|_C", "", colnames(gen))
@@ -57,21 +57,34 @@ map <- map[which(complete.cases(map[, 5:6])), ]
 gen <- gen[rownames(map), ]
 #------------------------------### Covariates ###-------------------------------
 cov <- read.table(file = file.path(inpath, "Cardiology_exptdes_bothphases.txt"),
-                    sep = "\t",
-                 header = TRUE
+                   sep = "\t",
+                header = TRUE)
+PROBE_ID.2 <- exp.raw.2[, 1]
+exp.2 <- exp.raw.2[, -1]
+colnames(exp.2) <- gsub("\\.[A-Z]*_[A-z]*", "", colnames(exp.2))
+colnames(exp.2) <- gsub("\\.", "_", colnames(exp.2))
+colnames(exp.2) <- cov[which(cov$ColumnName %in% colnames(exp.2)),"STUDY_ID"]
 # TODO: remove unnecessary columns
 # TODO: re-label columns for consistency across datasets
 #------------------------------### Cleanup ###----------------------------------
 names <- gen.id[which(gen.id %in% exp.id)]
 exp.1 <- exp.1[, names]           # only keep samples found in exp and gen data
+exp.1 <- exp.1[, order(colnames(exp.1))]
+exp.1 <- cbind(PROBE_ID.1, exp.1) # append probe IDs
+exp.2 <- exp.2[, order(colnames(exp.2))]
+exp.2 <- cbind(PROBE_ID.2, exp.2)
+colnames(exp.1)[1] <- "PROBE_ID"
+colnames(exp.2)[1] <- "PROBE_ID"
 gen   <- gen[, names]
-exp.1 <- cbind(PROBE_ID, exp.1)   # append probe IDs
+gen   <- gen[, order(colnames(gen))]
+gen   <- gen[order(rownames(gen)), ]
 gen   <- new("SnpMatrix", t(gen)) # create SnpMatrix object for output
 #----------------------------### Write to file ###------------------------------
-Write(exp.1, "CAD_exp.txt")
-Write(cov,  "CAD_cov.txt")
+Write(exp.1, "CAD_exp_phase1.txt")
+Write(exp.2, "CAD_exp_phase2.txt")
+Write(cov, "CAD_cov.txt")
 Write(info.probe, "CAD_probe_info.txt")
-write.plink(file.base = file.path(outpath, "genotypes/CAD"),
+write.plink(file.base = file.path(outpath, "genotypes/CAD_phase1"),
             snp.major = TRUE,
                  snps = gen,
          subject.data = fam,
