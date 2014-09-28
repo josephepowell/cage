dd051ac087f37770c4389e5d0b3899d327a64f75
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-29 08:48:45 +1000

    Change file output naming conventions, general clean up

--- cleanMorocco.R
+++ cleanMorocco.R
@@ -7,9 +7,8 @@ inpath  <- "/ibscratch/wrayvisscher/xander/CAGE/data/Morocco/raw"
 outpath <- "/ibscratch/wrayvisscher/xander/CAGE/data/Morocco/clean"
 source("/clusterdata/uqahollo/scripts/Write.R")
 #---------------------------### Expression data ###-----------------------------
-# Read raw expression data
 exp.raw  <- read.delim(file.path(inpath, "Group_Probe_Profile_Gibson_1-3_11-12-08.txt"), sep = "\t", fill = TRUE)
-# Extract probe labels and metadata from expression matrix
+# Extract probe labels and info from expression matrix
 PROBE_ID <- exp.raw[ ,3]
 exp      <- exp.raw[ ,4:length(exp.raw)]
 exp.info <- exp.raw[ ,1:2]
@@ -21,16 +20,13 @@ exp      <- exp[ ,order(colnames(exp))]
 exp.info <- cbind(PROBE_ID, exp.info)
 # Change column names for consistency between datasets
 colnames(exp.info)[2:3] <- c("TARGET_ID", "PROBE_NUMBER")
-
 #----------------------------### Genotype data ###------------------------------
-# Read raw genotype data
 gen.raw <- read.delim(file.path(inpath, "Morocco_194_genotypes_598k_tall.txt"), sep = "\t", header = TRUE)
 # Extract SNP labels
 RS_ID   <- gen.raw[ ,1]
 gen     <- gen.raw[ ,-1]
 # Sort samples by project-specific sample ID
 gen     <- gen[ ,order(colnames(gen))]
-
 #---------------------### Make data entries consistent ###----------------------
 # Remove samples which have only one of expression or genotype data entries
 cols <- which(!colnames(exp) %in% colnames(gen))
@@ -40,7 +36,6 @@ gen  <- gen[ ,-(cols)]
 # Insert platform-specific probe IDs
 exp  <- cbind(PROBE_ID, exp)
 gen  <- cbind(RS_ID, gen)
-
 #-----------------------------### PLINK files ###-------------------------------
 gen <- apply(gen, 2, function(x) ifelse(x == "--", "00", x))
 ped <- matrix(nrow = ncol(gen) - 1, ncol = 6 + nrow(gen))
@@ -68,12 +63,11 @@ map <- cbind(rep("0", nrow(gen)), # missing chromosomes
              rep("0", nrow(gen)),
              rep("0", nrow(gen)))
 #              alleles)
-
 #----------------------------### Write to file ###------------------------------
 if (all(colnames(exp)[-1] == colnames(gen)[-1])) {
-  Write(exp, "Morocco_expression_signals.txt")
+  Write(exp, "Morocco_exp.txt")
   Write(exp.info, "Morocco_probe_info.txt")
-  Write(gen, "genotypes/Morocco_genotypes.txt")
+  Write(gen, "genotypes/Morocco_gen.txt")
   write.table(ped, paste(outpath, "/genotypes/Morocco.ped", sep = ""), sep = "\t", eol = "\n",
               quote = FALSE, row.names = FALSE, col.names = FALSE)
   write.table(map, paste(outpath, "/genotypes/Morocco.map", sep = ""), sep = "\t", eol = "\n",
@@ -81,6 +75,5 @@ if (all(colnames(exp)[-1] == colnames(gen)[-1])) {
 } else {
   print("Error: Sample labelling is not consistent between expression and genotype data")
 }
-
 #------------------------------### Clean up ###---------------------------------
 #rm(inpath, outpath, PROBE_ID, RS_ID, names, cols, write, alleles, getAlleles)
