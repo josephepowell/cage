a32f6fc4621b9f33cb26b49ce36e0354570988ca
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-30 09:27:48 +1000

    Retain probe IDs as rownames

--- normalisation/LogNormalise.R
+++ normalisation/LogNormalise.R
@@ -3,10 +3,11 @@ LogNormalise <- function(x) {
   #   x: data to normalise.
   # Returns: Log transformed data.
   if (class(x[ ,1]) == "character") {
+    rownames(x) <- x[, 1]
     x <- x[, -1]
   }
-  x[x <= 0]    <- 10^(-10)
-  x[!is.na(x)] <- log2(x[!is.na(x)])
-  x[x < 0]     <- 0
+  x[x <= 0]    <- 10^(-10)           # manage negative and zero values
+  x[!is.na(x)] <- log2(x[!is.na(x)]) # ommit NA values
+  x[x < 0]     <- 0                  # zero negative values
   return(x)
 }
