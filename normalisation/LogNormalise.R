032c772ab8e2219c1385240b059630797f2379d9
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-30 15:47:43 +1000

    Add argument for removal of non-positive values

--- normalisation/LogNormalise.R
+++ normalisation/LogNormalise.R
@@ -1,12 +1,17 @@
-LogNormalise <- function(x) {
+LogNormalise <- function(x, neg.rm = TRUE) {
   # Log transform numerical data, ommitting NA values
   #   x: data to normalise.
+  #   neg.rm: logical, should non-positive values be set to NA.
   # Returns: Log transformed data.
-  if (class(x[ ,1]) == "character") {
+  if (class(x[, 1]) == "character") {
     rownames(x) <- x[, 1]
     x <- x[, -1]
   }
-  x[x <= 0]    <- 10^(-10)           # manage negative and zero values
+  if (neg.rm) {
+    x[x <= 0] <- NA       # remove negative and zero values
+  } else {
+    x[x <= 0] <- 10^(-10) # replace negative and zero values with small value
+  }
   x[!is.na(x)] <- log2(x[!is.na(x)]) # ommit NA values
   x[x < 0]     <- 0                  # zero negative values
   return(x)
