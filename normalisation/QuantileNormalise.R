8d680c2ef08291238d3f8ef778e5cd8b4abe2d35
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-30 16:01:43 +1000

    Change conditional to allow NAs in addition to positive values

--- normalisation/QuantileNormalise.R
+++ normalisation/QuantileNormalise.R
@@ -5,7 +5,7 @@ QuantileNormalise <- function(x) {
   # Returns: data.frame of numeric values, normalised across chips.
   stopifnot(require("limma"))
   x <- as.matrix(x)
-  if (!all(x >= 0)) {
+  if (!all(x[which(!is.na(x))] >= 0)) {
     stop("\n  x contains negative values. Perform log normalisation.")
   }
   x <- normalizeBetweenArrays(x)
