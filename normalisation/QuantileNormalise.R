91c37cca55cf962ff71c43ff91d501e5a9c509e6
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-10-01 09:24:36 +1000

    Add comments, simplify return of object

--- normalisation/QuantileNormalise.R
+++ normalisation/QuantileNormalise.R
@@ -3,12 +3,11 @@ QuantileNormalise <- function(x) {
   # package `limma`.
   #   x: data to normalise.
   # Returns: data.frame of numeric values, normalised across chips.
-  stopifnot(require("limma"))
-  x <- as.matrix(x)
+  stopifnot(require("limma")) # hard check for requisite package
+  x <- as.matrix(x)           # convert data to numerical matrix
   if (!all(x[which(!is.na(x))] >= 0)) {
     stop("\n  x contains negative values. Perform log normalisation.")
   }
-  x <- normalizeBetweenArrays(x)
-  x <- data.frame(x)
+  x <- data.frame(normalizeBetweenArrays(x))
   return(x)
 }
