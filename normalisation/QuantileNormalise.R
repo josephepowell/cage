bce14be99cfb509f56668c6b5e36adfb630bdd95
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-30 15:45:03 +1000

    Add check for package `limma`

--- normalisation/QuantileNormalise.R
+++ normalisation/QuantileNormalise.R
@@ -2,11 +2,13 @@ QuantileNormalise <- function(x) {
   # Perform quantile normalisation on numerical data using the Bioconductor
   # package `limma`.
   #   x: data to normalise.
-  # Returns: matrix of numeric values, normalised across chips.
+  # Returns: data.frame of numeric values, normalised across chips.
+  stopifnot(require("limma"))
   x <- as.matrix(x)
   if (!all(x >= 0)) {
     stop("\n  x contains negative values. Perform log normalisation.")
   }
-  x <- limma::normalizeBetweenArrays(x)
+  x <- normalizeBetweenArrays(x)
+  x <- data.frame(x)
   return(x)
 }
