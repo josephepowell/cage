fc58207ab94b510f1c4abb4d59abfb694d2627e7
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-09-30 14:10:26 +1000

    Add error message for negative values in quantile normalisation

--- normalisation/QuantileNormalise.R
+++ normalisation/QuantileNormalise.R
@@ -3,8 +3,10 @@ QuantileNormalise <- function(x) {
   # package `limma`.
   #   x: data to normalise.
   # Returns: matrix of numeric values, normalised across chips.
-  stopifnot(all(x >= 0))
-  # TODO: exit status for data that has not been log normalised
   x <- as.matrix(x)
-  x <- normalizeBetweenArrays(x))
+  if (!all(x >= 0)) {
+    stop("\n  x contains negative values. Perform log normalisation.")
+  }
+  x <- limma::normalizeBetweenArrays(x)
+  return(x)
 }
