a1b9d8f1babc01a39a24cb61fc48610095131d88
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-10-01 08:42:40 +1000

    Add argument for removal of probe ID character column

--- normalisation/LogNormalise.R
+++ normalisation/LogNormalise.R
@@ -1,17 +1,20 @@
-LogNormalise <- function(x, neg.rm = TRUE) {
+LogNormalise <- function(x, neg.rm = TRUE, row.names = TRUE) {
   # Log transform numerical data, ommitting NA values
   #   x: data to normalise.
   #   neg.rm: logical, should non-positive values be set to NA.
+  #   row.names: logical, does x contain a column of character values.
   # Returns: Log transformed data.
-  if (class(x[, 1]) == "character") {
-    rownames(x) <- x[, 1]
-    x <- x[, -1]
+  if (row.names) {
+    rownames(x) <- x[, 1] # x c
+    x           <- x[, -1]
   }
+
   if (neg.rm) {
     x[x <= 0] <- NA       # remove negative and zero values
   } else {
     x[x <= 0] <- 10^(-10) # replace negative and zero values with small value
   }
+
   x[!is.na(x)] <- log2(x[!is.na(x)]) # ommit NA values
   x[x < 0]     <- 0                  # zero negative values
   return(x)
