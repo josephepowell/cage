c2559b7c71179a9ee0ae2690875ea71dd0fee060
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-10-01 15:53:11 +1000

    Remove matrix coercion, add TODOs

--- normalisation/RemoveBatchEffects.R
+++ normalisation/RemoveBatchEffects.R
@@ -9,15 +9,18 @@ RemoveBatchEffects <- function(exp, process.info, sample.info) {
   #
   # Returns:
   #   
-  exp     <- as.matrix(exp)
-  prc.inf <- as.matrix(process.info)
-  smp.inf <- as.matrix(sample.info)
+  # TODO: add arguments for individual vectors of data
+  # TODO: check dimensions of all inputs
+  # TODO: create data.frames to store results
+  exp     <- exp
+  prc.inf <- process.info
+  smp.inf <- sample.info
   
   ex.date <- as.factor(prc.inf$RNA_EXTRACT_DATE)
   sen.id  <- as.factor(smp.inf$SENTRIX_ID)
   sen.pos <- as.factor(smp.inf$SENTRIX_POSITION)
 
-  mod <- lm(as.numeric(x[1, ]) ~ sen.id + sen.pos + ex.date)
+  mod <- lm(as.numeric(exp[1, ]) ~ sen.id + sen.pos + ex.date)
   fit <- summary(mod)
   # TODO: extract summary stats
 }
