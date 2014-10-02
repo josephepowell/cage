387e62fb0585b432af71f00981f2aa268a17e7c5
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-10-02 13:38:00 +1000

    Add coefficients output

--- normalisation/RemoveBatchEffects.R
+++ normalisation/RemoveBatchEffects.R
@@ -23,6 +23,8 @@ RemoveBatchEffects <- function(exp, process.info, sample.info) {
   exp.nrm <- matrix(nrow = nrow(exp),
                     ncol = ncol(exp),
                 dimnames = dimnames(exp))
+  coef    <- matrix(nrow = nrow(exp),
+                    ncol = 3)
 
   for (i in 1:nrow(exp)) {
     non.na <- array(!is.na(exp[i, ]))
@@ -31,11 +33,19 @@ RemoveBatchEffects <- function(exp, process.info, sample.info) {
       next
     }
 
-    mod.1  <- lm(as.numeric(exp[i, ]) ~ sen.id + sen.pos + ex.date)
-    fit.1  <- summary(mod.1)
+    mod.1 <- lm(as.numeric(exp[i, ]) ~ sen.id + sen.pos + ex.date)
+    fit.1 <- summary(mod.1)
     exp.nrm[i, non.na] <- fit.1$residuals
+
+    mod.2 <- lm(exp.nrm[i, ] ~ as.numeric(exp[i, ]))
+    fit.2 <- summary(mod.2)
+    coef[i, ] <- c(fit.2$coefficients[2], fit.2$r.squared, fit.1$r.squared)
   }
 
   exp.nrm <- data.frame(exp.nrm)
-  return(exp.nrm)
+  coef    <- data.frame(FIT_EST = coef[, 1],
+                         FIT_R2 = coef[, 2],
+                     CORRECT R2 = coef[, 3])
+  out <- list(exp.nrm, coef)
+  return(out)
 }
