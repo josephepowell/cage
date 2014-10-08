064e44f87a51b74b027908f8fa5ebbda385b794b
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-10-08 15:55:59 +1000

    Add `row.names` arg to check for character column

--- normalisation/QuantileNormalise.R
+++ normalisation/QuantileNormalise.R
@@ -3,12 +3,13 @@
 # Author: Alex Holloway
 # Date  : 30/09/2014
 #-------------------------------------------------------------------------------
-QuantileNormalise <- function(x) {
+QuantileNormalise <- function(x, row.names = FALSE) {
   # Perform quantile normalisation on numerical data using the Bioconductor
   # package `limma`.
   #
   # Args:
   #   x: data to normalise.
+  #   row.names: logical, does x contain a column of character values.
   #
   # Returns:
   #   data.frame of numeric values, normalised across chips.
@@ -17,6 +18,12 @@ QuantileNormalise <- function(x) {
   if (!all(x[which(!is.na(x))] >= 0)) {
     stop("\n  x contains negative values. Perform log normalisation.")
   }
+
+  if (row.names) {
+    rownames(x) <- x[, 1]  # move column of character entries to row.names
+    x           <- x[, -1] # strip character column
+  }
+
   x <- normalizeBetweenArrays(x)
   return(data.frame(x))
 }
