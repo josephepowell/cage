1d94542bef5af3d3e73b3dd411bd39bcf4c95d46
Alex Holloway 0434232571 <x4nd3r@users.noreply.github.com>
2014-10-09 11:54:18 +1000

    Implement function to correct for batch effects by principal components

---
+++ normalisation/CorrectByPca.R
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# File  : CorrectByPca.R
+# Author: Alex Holloway
+# Date  : 08/10/2014
+#-------------------------------------------------------------------------------
+CorrectByPca <- function(exp, pca) {
+  # Attempt to remove batch processing effects using principal components.
+  #
+  # Args:
+  #   exp: expression data.
+  #   pca: `pcaRes` object, returned by `ppca` function in `pcaMethods` package.
+  #
+  # Returns:
+  #   data.frame of expression levels, adjusted for principal component scores.
+  stopifnot(require("pcaMethods"))  # hard check for requisite package
+  exp.nrm <- matrix(nrow = nrow(exp),
+                    ncol = ncol(exp),
+                dimnames = dimnames(exp))
+
+  for (i in 1:nrow(exp)) {
+    non.na <- array(!is.na(exp[i, ]))
+    if (length(which(non.na)) < 16) {  # arbitrary threshold for NAs, prevents
+      exp.nrm[i, ] <- NA               # error during model construction
+      next
+    }
+    mod <- lm(as.numeric(exp[i, ]) ~ scores(pca)[, 1:24])
+    fit <- summary(mod)
+    exp.nrm[i, non.na] <- fit$residuals
+  }
+  exp.nrm <- data.frame(exp.nrm)
+}
